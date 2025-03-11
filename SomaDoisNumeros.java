c7e34e3ed1fa285dc823532f84a9a7d5d779f5e7
JOAO PAULO DE LIMA MARTINS <25.00806-1@maua.br>
2025-03-11 10:44:26 -0300

    Soma de números

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class SomaDoisNumeros {
+   public static void main(String[] args) {
+       double primeiroValor;
+       double segundoValor;
+       double resultado;
+       String textoUsuario = JOptionPane.showInputDialog("Digite o valor");
+       primeiroValor = Double.parseDouble(textoUsuario);
+       segundoValor = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor"));
+       resultado = primeiroValor + segundoValor;
+       String operacao = primeiroValor + " + " + segundoValor + " + " + resultado;
+       JOptionPane.showMessageDialog(null, "O resultado é " + resultado + "\n" + operacao);
+       
+       
+    }
+}
+    
