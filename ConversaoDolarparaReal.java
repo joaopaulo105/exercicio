6f06dc8a02f3a26aec570eb0f22f3d642fefe5e4
JOAO PAULO DE LIMA MARTINS <25.00806-1@maua.br>
2025-03-18 08:47:04 -0300

    Conversao Dolar para Real

--- ConversaoDolarparaReal.java
+++ ConversaoDolarparaReal.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ConversaoDolarparaReal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite a cotação do dólar: ");
+        System.out.print("Digite a cotação do dólar:");
         double cotacaoDolar = sc.nextDouble();
-        System.out.print("Digite a quantidade de dólares: ");
+        System.out.print("Digite a quantidade de dólares:");
         double quantidadeDolares = sc.nextDouble();
         double valorEmReais = cotacaoDolar * quantidadeDolares;
         System.out.println("O valor de " + quantidadeDolares + " dólares é R$ " + valorEmReais);
