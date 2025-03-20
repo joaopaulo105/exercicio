9fdeb6745a0afb397fbead7f4bc01e7b7377c0de
JOAO PAULO DE LIMA MARTINS <25.00806-1@maua.br>
2025-03-20 10:00:12 -0300

    Exemplo String

--- ExemploStrings.java
+++ ExemploStrings.java
@@ -4,13 +4,14 @@ public class ExemploStrings {
     public static void main(String[] args) {
         String nome = JOptionPane.showInputDialog("Qual o seu nome?");
         int idade = Integer.parseInt(JOptionPane.showInputDialog("Quantos anos você tem?"));
+        double peso = Double.parseDouble(JOptionPane.showInputDialog("Qual e o seu peso em kg?"));
                 
                 // concatenando com o operador +
-            JOptionPane.showMessageDialog(null,"Oi, " + nome + ".Você tem " + idade + " anos.");
+            JOptionPane.showMessageDialog(null,"Oi, " + nome + ". Você tem " + idade + " anos e seu peso é " + peso + "kg.");
                 
                 // "montando a string com o método format"
-                String s = String.format("Oi, %s. Você tem %d anos. Seu peso e %f.",
-                        nome, idade);
+                String s = String.format("Oi, %s. Você tem %d anos e seu peso e %.2f kg.",
+                        nome, idade, peso);
                 JOptionPane.showMessageDialog(null, s);
             }
         }
